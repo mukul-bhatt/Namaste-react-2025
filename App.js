6addb951ae80740da01aba43398245f317ffe220
Mukul Bhatt <mukulbhatt@Mukuls-MacBook-Air.local>
2025-08-05 18:29:11 +0530

    Laying the foundations

--- App.js
+++ App.js
@@ -1,6 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Hello world form React! this is me testing my app now the goal is to implement the scripts for runnign our app in package .josn");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Mukul bhatt is here with React.createElement"
+);
+
+const Title = (
+  <div>
+    <h1>This is title component 🚀</h1>
+  </div>
+);
+
+const header = (
+  <>
+    <h1>This is h3 </h1>
+    <h2>This is h2</h2>
+    <h3>This is h1</h3>
+  </>
+);
+
+const HeadingComponent = () => (
+  <>
+    {header}
+    <h1 id="jsxHeading" className="patani">
+      This is a jsxHeading
+    </h1>
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<HeadingComponent />);
